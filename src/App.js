8631c018364b64eb303b3767ba8d77ad3ae00105
suraj gharti magar <suraajgm@gmail.com>
2022-05-11 16:03:17 +0545

    First react with mapbox-gl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Map, { Marker } from "react-map-gl";
+import mapMarker from "./marker/mapMarker.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Map
+        initialViewState={{
+          longitude: 87.27721567619342,
+          latitude: 26.449432670637346,
+          zoom: 8,
+          pitch: 50,
+        }}
+        style={{ width: 800, height: 400 }}
+        mapStyle="mapbox://styles/ankur20/cklq3agbe6tc918ny8hlc7ch6"
+        mapboxAccessToken="pk.eyJ1IjoiYW5rdXIyMCIsImEiOiJja2tiOW4wNGIwNDh5MnBsY3EzeDNmcTV4In0.d4LelcSFDElA3BctgWvs1A"
+      >
+        <Marker
+          latitude={26.449432670637346}
+          longitude={87.27721567619342}
+          anchor="bottom"
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src={mapMarker}
+            alt="mapMarker"
+            style={{ width: 25, height: 25 }}
+          />
+        </Marker>
+      </Map>
     </div>
   );
 }
